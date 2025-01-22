609779e677a66b65fe33800a4dda17734c96e1be
schmynth <service-noreply@gmx.de>
2025-01-22 13:42:46 +0100

    updated installPackages script

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 my linux dotfiles containing config files for:
 
 vim, kitty, hyprland, waybar...
+
+## Requirements
+
+### pacman
+
+You need to activate multilib repo in /etc/pacman.conf
--- installPackages.sh
+++ installPackages.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sudo pacman -S steam reflector wine-staging winetricks yabridge ddcutils i2c-tools ncdu gamemode gamescope
-# packages for hyprland: wofi hyprlock rofi hyprpaper alacritty ttf-font-awesome dunst
-yay -S x11-keyboard-flags bitwig-studio coolercontrol coolercontrold cpupower-gui extension-manager opencl-amd renoise visual-studio-code-bin
+set -x
+# install pacman packages
+sudo pacman -S - < .packagesPacman
+
+# install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+yay -S --needed - < .packagesAUR 
